c9bf997f56131386f614b44804613390e68a56f8
yekim <duddms9811@naver.com>
2024-04-24 17:05:10 +0900

    __main__ 체크 해제

--- DAG/test.py
+++ DAG/test.py
@@ -22,28 +22,27 @@ from datetime import datetime, timedelta
 # 	last_updated_on	TIMESTAMP NULL
 # );
 
-if __name__ == '__main__':
-    id = uuid4().__str__()
-    now = datetime.now().__str__()
-    default_args = {
-        'start_date': datetime.today()
-    }
+id = uuid4().__str__()
+now = datetime.now().__str__()
+default_args = {
+    'start_date': datetime.today()
+}
 
-    with DAG(
-        'insert_alarm',
-        default_args = default_args,
-        description = """
-            insert data to 'alarms' table
-        """,
-        schedule_interval = timedelta(minutes=3),  # scheduler interval 설정
-        start_date = datetime(2024, 4, 24),
-        catchup = False,    # 과거 실행에서 누락된 작업 재실행 여부
-        tags = ['mysql', 'test', 'alarms']
-    ) as dag:
-        t2 = MySqlOperator(
-            task_id="insert_employees_data",
-            mysql_conn_id="mysql",
-            sql='insert into `alarms` values ('+id+',`000164ff-ce67-4836-a219-0857d7530e76`, `test_title`, `test_content`, `test_link`, '+now+', '+now+' );'
-        )
+with DAG(
+    'insert_alarm',
+    default_args = default_args,
+    description = """
+        insert data to 'alarms' table
+    """,
+    schedule_interval = timedelta(minutes=3),  # scheduler interval 설정
+    start_date = datetime(2024, 4, 24),
+    catchup = False,    # 과거 실행에서 누락된 작업 재실행 여부
+    tags = ['mysql', 'test', 'alarms']
+) as dag:
+    t2 = MySqlOperator(
+        task_id="insert_employees_data",
+        mysql_conn_id="mysql",
+        sql='insert into `alarms` values ('+id+',`000164ff-ce67-4836-a219-0857d7530e76`, `test_title`, `test_content`, `test_link`, '+now+', '+now+' );'
+    )
 
-        t2
+    t2
