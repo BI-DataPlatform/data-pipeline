2d99ca1c8d96ff3414e2787af59b5d66ae445ad8
yekim <duddms9811@naver.com>
2024-04-25 14:12:09 +0900

    account에서 랜덤 id를 하나 가져와 사용하게 변경 / 디버깅용 중간에 task 결과 print 추가

--- DAG/insert_alarm.py
+++ DAG/insert_alarm.py
@@ -1,9 +1,10 @@
 from airflow.models import DAG
 from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.operators.python import PythonOperator
 
 from uuid import uuid4
 from datetime import datetime, timedelta
-
+import random
 # schema는 mysql cli로 생성
 # CREATE TABLE alarms (
 # 	id VARCHAR(64) NOT NULL,
@@ -28,6 +29,7 @@ default_args = {
     'start_date': datetime.today()
 }
 
+
 with DAG(
     'insert_alarm',
     default_args = default_args,
@@ -35,14 +37,32 @@ with DAG(
         insert data to 'alarms' table
     """,
     schedule_interval = timedelta(minutes=3),  # scheduler interval 설정
-    start_date = datetime(2024, 4, 24),
     catchup = False,    # 과거 실행에서 누락된 작업 재실행 여부
     tags = ['mysql', 'test', 'alarms']
 ) as dag:
-    t2 = MySqlOperator(
-        task_id="insert_alarm_data",
+    
+    t1 = MySqlOperator(
+        task_id="select_random_account_data",
         mysql_conn_id="mysql",
-        sql="insert into alarms values ('"+id+"','000164ff-ce67-4836-a219-0857d7530e76', 'test_title', 'test_content', 'test_link', '"+now+"', '"+now+"' );"
+        sql="SELECT id FROM accounts ORDER BY RAND() LIMIT 1;",
+        result_set_task_id="account_data"
     )
 
-    t2
+
+    def print_account_data(**context):
+        account_data = context['task_instance'].xcom_pull(task_ids="select_random_account_data", key="return_value")
+        print("Random account data:", account_data)
+
+    t2 = PythonOperator(
+        task_id="print_account_data",
+        python_callable=print_account_data,
+        provide_context=True  # context 제공을 위한 옵션
+    )
+
+    t3 = MySqlOperator(
+        task_id="insert_alarm_data",
+        mysql_conn_id="mysql",
+        sql=f"insert into alarms values ('{id}', '{{ task_instance.xcom_pull(task_ids=\"select_account_data\", key=\"return_value\")[0][0] }}', 'test_title', 'test_content', 'test_link', '{now}', '{now}' );"
+    )
+
+    t1 >> t2 >> t3
